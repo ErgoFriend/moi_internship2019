546d3bd6308b7644d872dd4a385a90bc7e57d209
ergop <ergop@Kasu.local>
2019-07-03 23:30:59 +0900

    util

--- util/search.go
+++ util/search.go
@@ -11,7 +11,7 @@ import (
 
 var symbols = []string{"+", "-", "*", "/"}
 
-func Eval(formula, answer, quizID string) {
+func eval(formula, answer, quizID string) {
 	// fmt.Printf("%+v\n", formula)
 	expression, _ := govaluate.NewEvaluableExpression(formula)
 	result, _ := expression.Evaluate(nil)
@@ -27,72 +27,52 @@ func Eval(formula, answer, quizID string) {
 func EvalFormulas(strQuiz, answer, quizID string, level int) {
 	wg := &sync.WaitGroup{} // WaitGroupの値を作る
 	fmt.Printf("EvalFormulas\n")
-	if level == 2 {
-		for _, v := range symbols {
-			wg.Add(1) // wgをインクリメント
-			go func(replaced1 string) {
-				for _, j := range symbols {
-					wg.Add(1) // wgをインクリメント
-					go func(replaced2 string) {
-						for _, i := range symbols {
-							wg.Add(1) // wgをインクリメント
-							go func(replaced3 string) {
-								for _, k := range symbols {
-									wg.Add(1) // wgをインクリメント
-									go func(formula string) {
-										Eval(formula, answer, quizID)
-										// fmt.Printf("formula: %+v\n", formula)
-										wg.Done()
-									}(strings.Replace(replaced3, "?", k, 1))
-								}
-								wg.Done()
-							}(strings.Replace(replaced2, "?", i, 1))
-						}
-						wg.Done()
-					}(strings.Replace(replaced1, "?", j, 1))
-				}
-				wg.Done()
-			}(strings.Replace(strQuiz, "?", v, 1))
-		}
-	} else if level == 3 {
-		for _, v := range symbols {
-			wg.Add(1)
-			go func(replaced1 string) {
-				for _, j := range symbols {
-					wg.Add(1)
-					go func(replaced2 string) {
-						for _, i := range symbols {
-							wg.Add(1)
-							go func(replaced3 string) {
-								for _, k := range symbols {
-									wg.Add(1)
-									go func(replaced4 string) {
-										for _, l := range symbols {
-											wg.Add(1)
-											go func(replaced5 string) {
-												for _, m := range symbols {
-													wg.Add(1) // wgをインクリメント
-													go func(formula string) {
-														Eval(formula, answer, quizID)
-														wg.Done()
-													}(strings.Replace(replaced5, "?", m, 1))
-												}
-												wg.Done()
-											}(strings.Replace(replaced4, "?", l, 1))
-										}
-										wg.Done()
-									}(strings.Replace(replaced3, "?", k, 1))
-								}
-								wg.Done()
-							}(strings.Replace(replaced2, "?", i, 1))
-						}
-						wg.Done()
-					}(strings.Replace(replaced1, "?", j, 1))
-				}
-				wg.Done()
-			}(strings.Replace(strQuiz, "?", v, 1))
-		}
+	for _, v := range symbols {
+		wg.Add(1) // wgをインクリメント
+		go func(replaced1 string) {
+			for _, j := range symbols {
+				wg.Add(1) // wgをインクリメント
+				go func(replaced2 string) {
+					for _, i := range symbols {
+						wg.Add(1) // wgをインクリメント
+						go func(replaced3 string) {
+							for _, k := range symbols {
+								wg.Add(1) // wgをインクリメント
+								go func(replaced4 string) {
+
+									// if level == 3 {
+									// level3だけに対応
+									for _, l := range symbols {
+										wg.Add(1)
+										go func(replaced5 string) {
+											for _, m := range symbols {
+												wg.Add(1) // wgをインクリメント
+												go func(formula string) {
+													eval(formula, answer, quizID)
+													wg.Done()
+												}(strings.Replace(replaced5, "?", m, 1))
+											}
+											wg.Done()
+										}(strings.Replace(replaced4, "?", l, 1))
+									}
+									wg.Done()
+									// } else {
+									// 	eval(replaced4, answer, quizID)
+									// 	wg.Done()
+									// }
+
+								}(strings.Replace(replaced3, "?", k, 1))
+							}
+							wg.Done()
+						}(strings.Replace(replaced2, "?", i, 1))
+					}
+					wg.Done()
+				}(strings.Replace(replaced1, "?", j, 1))
+			}
+			wg.Done()
+		}(strings.Replace(strQuiz, "?", v, 1))
 	}
+
 	wg.Wait()
 	fmt.Printf("EvalFormulas Done.\n")
 }
